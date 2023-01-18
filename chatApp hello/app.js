ebde88d0cfc2105084cdbc7f79c87669981134be
Sayak Raha <sayakraha12@gmail.com>
2023-01-18 17:02:55 +0530

    chat app incomplete commit 2

--- chatApp hello/app.js
+++ chatApp hello/app.js
@@ -15,5 +15,33 @@ cover.addEventListener("mousemove", (e)=>{
     const percy = (e.clientY/cover.offsetHeight)*100;
     cover.style.setProperty("--percx", percx+ "%");
     cover.style.setProperty("--percy", percy+ "%");
-
 })
+const userbtn = document.querySelectorAll(".signbtn")
+window.onresize =()=>{
+    if (window.innerWidth <= 1150) {
+        userbtn.forEach((userbtn)=>{
+            userbtn.classList.add("active");
+        })
+        document.querySelector(".signin").style.display = "none";
+        document.querySelector(".signup").style.display = "none";
+        document.querySelectorAll(".menuitem").forEach((item)=>{
+            item.style.display = "none";
+        })
+        document.querySelector(".menu").style.position = "absolute";
+
+        document.querySelector(".menu").style.right = "15%";
+
+    }
+    else{
+        userbtn.forEach((userbtn)=>{
+            userbtn.classList.remove("active");
+        })
+        document.querySelector(".signin").style.display = "flex";
+        document.querySelector(".signup").style.display = "flex";
+        document.querySelectorAll(".menuitem").forEach((item)=>{
+            item.style.display = "flex";
+        })
+        document.querySelector(".menu").style.position = "unset";
+
+    }
+}
