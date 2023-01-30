e6a239179b18f806e1bd95035b2787aa1c64e081
Sayak Raha <sayakraha12@gmail.com>
2023-01-30 17:26:13 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,75 @@
+const navbar = document.querySelector(".nav");
+navbar.addEventListener("mousemove", (e)=>{
+    const percent = (e.clientX/navbar.offsetWidth)*100;
+    const basestartpercent = 40,
+    baseendpercent = 60,
+    startpercent = percent -20,
+    endpercent = percent + 20;
+    navbar.style.setProperty("--startperc", startpercent + "%");
+    navbar.style.setProperty("--endperc", endpercent + "%");
+
+})
+const cover = document.querySelector(".coverdiv");
+cover.addEventListener("mousemove", (e)=>{
+    const percx = (e.clientX/cover.offsetWidth)*100;
+    const percy = (e.clientY/cover.offsetHeight)*100;
+    cover.style.setProperty("--percx", percx+ "%");
+    cover.style.setProperty("--percy", percy+ "%");
+})
+const userbtn = document.querySelectorAll(".signbtn")
+window.onload =()=>{
+    if (window.innerWidth <= 1150) {
+        userbtn.forEach((userbtn)=>{
+            userbtn.classList.add("active");
+        })
+        document.querySelector(".signin").style.display = "none";
+        document.querySelector(".signup").style.display = "none";
+        document.querySelectorAll(".menuitem").forEach((item)=>{
+            item.style.display = "none";
+        })
+        document.querySelector(".menu").style.position = "absolute";
+
+        document.querySelector(".menu").style.right = "10%";
+
+    }
+    else{
+        userbtn.forEach((userbtn)=>{
+            userbtn.classList.remove("active");
+        })
+        document.querySelector(".signin").style.display = "flex";
+        document.querySelector(".signup").style.display = "flex";
+        document.querySelectorAll(".menuitem").forEach((item)=>{
+            item.style.display = "flex";
+        })
+        document.querySelector(".menu").style.position = "unset";
+
+    }
+}
+window.onresize =()=>{
+    if (window.innerWidth <= 1150) {
+        userbtn.forEach((userbtn)=>{
+            userbtn.classList.add("active");
+        })
+        document.querySelector(".signin").style.display = "none";
+        document.querySelector(".signup").style.display = "none";
+        document.querySelectorAll(".menuitem").forEach((item)=>{
+            item.style.display = "none";
+        })
+        document.querySelector(".menu").style.position = "absolute";
+
+        document.querySelector(".menu").style.right = "10%";
+
+    }
+    else{
+        userbtn.forEach((userbtn)=>{
+            userbtn.classList.remove("active");
+        })
+        document.querySelector(".signin").style.display = "flex";
+        document.querySelector(".signup").style.display = "flex";
+        document.querySelectorAll(".menuitem").forEach((item)=>{
+            item.style.display = "flex";
+        })
+        document.querySelector(".menu").style.position = "unset";
+
+    }
+}
